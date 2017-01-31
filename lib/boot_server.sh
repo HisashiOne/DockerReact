2cd2b03085f3252098311cecbb018b22ee542f2b
Oswaldo Morales <HisashiOne@MacBook-Pro-Oswaldo.local>
2017-01-31 13:08:49 -0600

    Docker React

---
+++ lib/boot_server.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /var/www/html/react-tutorial-master
+npm install
+npm install -g browserify
+npm install -g watchify
+npm install react
+npm install react-dom
+npm install reactify
+node server.js
